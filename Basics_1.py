e731de603599bee68af28ee7c6f6ee48012e6112
rahulneon <rahul15neelakantam@gmail.com>
2026-02-23 07:41:07 +0530

    updated with revision

--- Basics_1.py
+++ Basics_1.py
@@ -1,1 +1,130 @@
 print("yoyo")
+print("Hi this is Rahul K, this is my learning tutorials.")
+print("Hi this is Rahul K\n"," This is my learning tutorials.")
+print()
+print(123+34)
+
+myName = "Rahul"
+myAge= 36
+price = 25.99
+print("my age is ",myAge)
+print("my name is ",myName)
+print("the stock price of",myName," is",price)
+myNewAge= myAge
+print("my new age is", myNewAge)
+myNewAge=37
+print("my new age is",myNewAge)
+print(type(myNewAge))
+print(type(myName))
+print(type(price))
+
+name1='Rahul'
+name2="Pravalika"
+name3='''Bhuvan'''
+print(name1)
+print(name2)
+print(name3)
+
+myFLoat =12.343
+print(myFLoat)
+
+mybool =True
+print(mybool)
+print(type(mybool))
+temperature =None
+print(type(temperature))
+
+#print(learning to comment)
+
+# check on the license
+# but with a multiline comment with ctrl+/
+
+'''
+print(learning to comment)
+check on the license
+but with a multiline comment with ctrl+/
+'''
+
+#Arthematic operations
+numInputA= 5
+numInputB =100
+print(numInputB+numInputA)
+print(numInputA-numInputB)
+print(numInputB/numInputA)
+print(numInputB*numInputA)
+numInputA=3
+print(numInputB%numInputA) #modulo or remainder
+print(numInputA%numInputB) #modulo or remainder
+print(numInputA**2) #power
+print(
+
+)
+# checking on relational operation
+print(numInputA==numInputB)
+print(numInputA>numInputB)
+print(numInputA<numInputB)
+print(numInputA>=numInputB)
+print(numInputA<=numInputB)
+print(
+    )
+numInputB=3
+print(numInputA>=numInputB)
+print(numInputA<=numInputB)
+numInputB += 97
+print(numInputB)
+numRes =numInputB
+numRes *=100
+print(numRes)
+numRes %=10
+print(numRes)
+numRes -=10
+print(numRes)
+
+# logical operations
+print(
+    
+)
+print(True)
+print(False)
+print(not(True))
+print(not(False))
+print(not(numInputA>numInputB))
+print(not(numInputA<numInputB))
+print(
+
+)
+val1 =True
+val2= False
+# logical operatiors
+print(val1 and val2)
+print(val1 or val2)
+
+#type conversion and type casting
+a= 23
+b=27.190
+sum= a+b  # implicit conversion or type converison as  a becomes float here
+print(type(a))
+print(type(sum))
+print(sum)
+c="32423"
+print(type(c))
+d="232.424"
+print(type(d))
+e=float(c)
+print(type(e))
+e=int(e)
+print(type(e))
+f=float(d)
+print(f)
+myNewName = input("enter your name")
+print("my new name is",myNewName)
+print(type(myNewName))
+
+#as input considers all values into  string now to  typecast to int
+
+newData =float(input("enter a float value only"))
+print(newData)
+newDataName =input("enter a name value characters only \t")
+print(newDataName)
+newDataAge =int(input("enter a age value int only\t"))
+print(newDataAge)
